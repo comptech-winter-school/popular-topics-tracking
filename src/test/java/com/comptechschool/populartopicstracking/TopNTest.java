0e9fd00c5a4ddf3410da89b7a0db6920d4b57551
Popov-Dmitry <popov.dmitry.dev@gmail.com>
2022-01-31 16:56:35 +0700

    Removed TopNStreamProcess and added TopNTest

--- src/test/java/com/comptechschool/populartopicstracking/TopNTest.java
+++ src/test/java/com/comptechschool/populartopicstracking/TopNTest.java
@@ -1,49 +1,69 @@
-package com.comptechschool.populartopicstracking.operator.topn;
+package com.comptechschool.populartopicstracking;
 
-import com.comptechschool.populartopicstracking.DataSource;
 import com.comptechschool.populartopicstracking.entity.InputEntity;
+import com.comptechschool.populartopicstracking.operator.InputEntityFilter;
+import com.comptechschool.populartopicstracking.operator.topn.EntityProcessFunction;
+import com.comptechschool.populartopicstracking.operator.topn.EntityTrigger;
 import org.apache.flink.api.common.eventtime.WatermarkStrategy;
 import org.apache.flink.api.common.restartstrategy.RestartStrategies;
+import org.apache.flink.api.java.functions.KeySelector;
 import org.apache.flink.streaming.api.CheckpointingMode;
-import org.apache.flink.streaming.api.datastream.DataStreamSource;
-import org.apache.flink.streaming.api.datastream.SingleOutputStreamOperator;
 import org.apache.flink.streaming.api.environment.CheckpointConfig;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.streaming.api.functions.sink.PrintSinkFunction;
 import org.apache.flink.streaming.api.windowing.assigners.TumblingEventTimeWindows;
 import org.apache.flink.streaming.api.windowing.time.Time;
+import org.junit.Test;
 
 import java.time.Duration;
-import java.util.List;
 
+public class TopNTest {
 
-/**
- * TopN counts requests in real time
- */
-public class TopNStreamProcess {
+    @Test
+    public void topNTest() throws Exception {
+        int n = 3;
 
-    private static int topN = 3;
-
-    public void runTopNOperator() throws Exception {
         StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
         initProperties(env);
-        DataStreamSource<InputEntity> source = env.addSource(new DataSource(10000L));
-        SingleOutputStreamOperator<List<InputEntity>> dataStream = source
+
+        env.addSource(new DataSource(10000L))
                 //.assignTimestampsAndWatermarks(new EntityAssignerWaterMarks(Time.seconds(5)))
                 .assignTimestampsAndWatermarks(WatermarkStrategy.forBoundedOutOfOrderness(Duration.ofSeconds(20)))
                 .windowAll(TumblingEventTimeWindows.of(Time.seconds(30)))
                 .allowedLateness(Time.seconds(20))
                 .trigger(new EntityTrigger(50000))//clean up the window data
-                .process(new EntityProcessFunction(topN));
+                .process(new EntityProcessFunction(n))
+                .addSink(new PrintSinkFunction<>());
 
-        dataStream.print();
-        // TODO Sink
+        // TODO Cassandra Sink
         env.execute("Real-time entity topN");
     }
 
+    @Test
+    public void filterKeyByTopNTest() throws Exception {
+        int n = 3;
+
+        StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+        initProperties(env);
+
+        env.addSource(new DataSource(10000L))
+                //.filter(new InputEntityFilter())
+                //.keyBy(...)
+                //.assignTimestampsAndWatermarks(new EntityAssignerWaterMarks(Time.seconds(5)))
+                .assignTimestampsAndWatermarks(WatermarkStrategy.forBoundedOutOfOrderness(Duration.ofSeconds(20)))
+                .windowAll(TumblingEventTimeWindows.of(Time.seconds(30)))
+                .allowedLateness(Time.seconds(20))
+                .trigger(new EntityTrigger(50000))//clean up the window data
+                .process(new EntityProcessFunction(n))
+                .addSink(new PrintSinkFunction<>());
+
+        // TODO Cassandra Sink
+        env.execute("Real-time entity topN");
+    }
 
     private void initProperties(StreamExecutionEnvironment env) {
         //Global parallelism
-        env.setParallelism(1);
+        env.setParallelism(5);
 
         //checkpoint per minute
         env.enableCheckpointing(1000 * 60 * 10);
@@ -57,7 +77,5 @@ public class TopNStreamProcess {
         //Do not delete the save point data even if you manually cancel the task
         env.getCheckpointConfig().setExternalizedCheckpointCleanup(CheckpointConfig.
                 ExternalizedCheckpointCleanup.RETAIN_ON_CANCELLATION);
-
-
     }
 }
