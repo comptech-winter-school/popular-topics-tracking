e157b333659e4553732884cc55d2746e0e3089b7
eugene <ekimenko_evgeniyy@mail.ru>
2022-02-03 22:14:15 +0300

    Added Identifier entity class

---
+++ src/main/java/com/comptechschool/populartopicstracking/Identifier.java
@@ -0,0 +1,19 @@
+package com.comptechschool.populartopicstracking;
+
+import com.datastax.driver.mapping.annotations.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.cassandra.core.mapping.PrimaryKey;
+
+import java.io.Serializable;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "testdb")
+public class Identifier implements Serializable {
+    @PrimaryKey
+    private Long id;
+    private String name;
+}
